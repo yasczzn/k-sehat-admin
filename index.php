458dc0f4359aacdf4942f9c2a77d6e9fd3a8f47e
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-01-31 21:42:40 +0700

    310123
    edit 2 files, delete user index.php

--- index.php
+++ index.php
@@ -1,321 +1,95 @@
+<?php 
+ 
+    include '../function/connection.php';
+    
+    error_reporting(0);
+    
+    session_start();
+    
+    if (isset($_SESSION['username'])) {
+        header("Location: forms/admin-dashboard.php");
+    }
+    
+    if (isset($_POST['submit'])) {
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+        $password = md5(filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
+    
+        $sql = "SELECT * FROM user_admin WHERE email='$email' AND password='$password'";
+        $result = mysqli_query($conn, $sql);
+        if ($result->num_rows > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['username'] = $row['username'];
+            echo "<script>alert('Login successful!')</script>";
+            header("Location: admin-dashboard.php");
+        } else {
+            echo "<script>alert('Invalid email or password. Please try again!')</script>";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Keluarga Sehat Admin - Login</title>
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+            <!-- Favicons -->
+        <link href="../assets/img/keluargasehat.png" rel="icon">
+        <link href="../assets/img/keluargasehat.png" rel="apple-touch-icon">
 
-  <title>Keluarga Sehat Website</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/keluargasehat.png" rel="icon">
-  <link href="assets/img/keluargasehat.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="../assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Scaffold - v4.10.0
-  * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center">
-
-      <div class="logo me-auto">
-        <h1><a href="#">Keluarga Sehat<img src="assets/img/keluargasehat.png" alt="" class="img-fluid"></a></h1>
-      </div>
-
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
-          <li><a class="nav-link scrollto" href="#features">Why Us?</a></li>
-          <li><a class="nav-link scrollto" href="#pricing">Our Services</a></li>
-          <li><a class="nav-link scrollto" href="#footer">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-    </div>
-  </header><!-- End Header -->
-
-  <!-- ======= Hero Section ======= -->
-  <section id="hero">
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="fade-up">
-          <div>
-            <h1>Keluarga Sehat helps you to track your vaccination history</h1>
-            <h2>Together we can eradict Covid-19</h2>
-            <a href="#about" class="btn-get-started scrollto">Find out more</a>
-          </div>
-        </div>
-        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
-          <img src="assets/img/hero-img.svg" class="img-fluid" alt="">
-        </div>
-      </div>
-    </div>
-
-  </section><!-- End Hero -->
-
-  <main id="main">
-
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-lg-6" data-aos="zoom-in">
-            <img src="assets/img/about.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 d-flex flex-column justify-contents-center" data-aos="fade-left">
-            <div class="content pt-4 pt-lg-0">
-              <h3>Learn more about us</h3>
-              <p class="fst-italic">
-                Keluarga Sehat Website is an online vaccine registration platform where users can register for vaccines through an online website from various devices anywhere and anytime. User need to book a vaccination schedule by fill a registration form. Patient need to insert their personal data and select the vaccination type they want to book in. After submit the registration form, patient will get an email to continue the payment and print the vaccination ticket.
-              </p>
-              <ul>
-                <li><i class="bi bi-check-circle"></i> No Queueing, no crowds.</li>
-                <li><i class="bi bi-check-circle"></i> No long waiting for result</li>
-                <li><i class="bi bi-check-circle"></i> Accessible anytime, anywere</li>
-              </ul>
-              <p>
-                Enjoy the benefits and reliabilities of keluarga sehat!
-              </p>
+        <link href="assets/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-prior">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-sm-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Admin Login</h3></div>
+                                    <div class="card-body">
+                                        <form action="" method="POST">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" name="email" value="<?php echo $email; ?>" type="email" placeholder="name@example.com" required/>
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" name="password" value="<?php echo $_POST['password']; ?>" type="password" placeholder="Password" required/>
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <input class="btn btn-primary" value="Login" type="submit" name="submit">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End About Section -->
-
-    <!-- ======= Features Section ======= -->
-    <section id="features" class="features">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-lg-6 mt-2 mb-tg-0 order-2 order-lg-1">
-            <ul class="nav nav-tabs flex-column">
-              <li class="nav-item" data-aos="fade-up">
-                <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">
-                  <h4>Track your vaccination progress!</h4>
-                  <p>Your current vaccination status will be shown in your digital ID's vaccination description.</p>
-                </a>
-              </li>
-              <li class="nav-item mt-2" data-aos="fade-up" data-aos-delay="100">
-                <a class="nav-link" data-bs-toggle="tab" href="#tab-2">
-                  <h4>No Queueing!</h4>
-                  <p>Provide social distancing and time-saving for register a vaccination schedule.</p>
-                </a>
-              </li>
-              <li class="nav-item mt-2" data-aos="fade-up" data-aos-delay="200">
-                <a class="nav-link" data-bs-toggle="tab" href="#tab-3">
-                  <h4>Easy access to any places!</h4>
-                  <p>Every crowded and public place can get your vaccination status by scaning your ID.</p>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in">
-            <div class="tab-content">
-              <div class="tab-pane active show" id="tab-1">
-                <figure>
-                  <img src="assets/img/features-1.svg" alt="" class="img-fluid">
-                </figure>
-              </div>
-              <div class="tab-pane" id="tab-2">
-                <figure>
-                  <img src="assets/img/features-2.svg" alt="" class="img-fluid">
-                </figure>
-              </div>
-              <div class="tab-pane" id="tab-3">
-                <figure>
-                  <img src="assets/img/features-3.svg" alt="" class="img-fluid">
-                </figure>
-              </div>
-              <div class="tab-pane" id="tab-4">
-                <figure>
-                  <img src="assets/img/features-4.svg" alt="" class="img-fluid">
-                </figure>
-              </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Putri Yasmina 2022-<?php echo date("Y");?></div>
+                            <div>
+                                <a href="https://github.com/yasczzn">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
-          </div>
         </div>
-
-      </div>
-    </section><!-- End Features Section -->
-
-    <!-- ======= Pricing Section ======= -->
-    <section id="pricing" class="pricing section-bg">
-      <div class="container">
-
-        <div class="section-title" data-aos="fade-up">
-          <h2>Vaccination Packages</h2>
-          <p>Take your friends/family to vaccinate and get wholesome discount bundles! </p>
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6">
-            <div class="box" data-aos="zoom-in">
-              <h3>Single package</h3>
-              <h4>Free</h4>
-              <ul>
-                <li>For 1 person</li>
-                <li>Pay for vaccination price only</li>
-                <li></li>
-                <li class="na">Discount for the next vaccination</li>
-                <li class="na">Get reward points for out other services</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="user/regist-1.php" class="btn-buy">Book Now</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-            <div class="box featured" data-aos="zoom-in" data-aos-delay="100">
-              <h3>Duo package</h3>
-              <h4><sup>+ Rp</sup>10000<span> / person</span></h4>
-              <ul>
-                <li>For 2 people</li>
-                <li>Pay for vaccination price and addition price/person</li>
-                <li>Get 20% off for next vaccination</li>
-                <li></li>
-                <li class="na">Get reward points for out other services</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="user/regist-1.php" class="btn-buy">Book Now</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-            <div class="box" data-aos="zoom-in" data-aos-delay="300">
-              <span class="advanced">Best deal!</span>
-              <h3>Family package</h3>
-              <h4><sup>+ Rp</sup>15000<span> / person</span></h4>
-              <ul>
-                <li>Up to 6 people</li>
-                <li>Pay for vaccination price and addition price/person</li>
-                <li>Get 25% off for next vaccination</li>
-                <li>40% discount coupon for our other services for 100 lucky patient</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="user/regist-1.php" class="btn-buy">Book Now</a>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Pricing Section -->
-
-    <!-- ======= Services Section ======= -->
-    <section id="cta" class="cta section-bg">
-      <div class="container">
-
-        <div class="section-title" data-aos="fade-up">
-          <h2>Have You vaccinated yet? <br> Register now!</h2>
-          <p>Register in Keluarga Sehat to save your vaccination progress!</p>
-          <a href="user/regist-1.php" class="cta-btn scrollto">Register Now</a>
-        </div>
-
-      </div>
-    </section><!-- End Services Section -->
-
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3>Keluarga Sehat Website</h3>
-              <h5></h5>
-              <p>
-                Jl. Merbabu IV Rawa Mekar<br>
-                South Tangerang, Indonesia<br><br>
-                <strong>Phone:</strong> +62 1234 5678 901<br>
-                <strong>Email:</strong> ksehat@gmail.com<br>
-              </p>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Sections</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#about">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#features">Why Us?</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#pricing">Our Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#footer">Contact</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Partnerships</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.kemkes.go.id">Official Site</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://vaksin.kemkes.go.id/#/vaccines">Vaccination Data Site</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://covid19.go.id/peta-sebaran">Covid Spread Map</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Scaffold</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" title="to top" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="../assets/js/scripts.js"></script>
+    </body>
 </html>
