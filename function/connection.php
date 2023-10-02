d847604daddb90c02095fdefa424ff869176637e
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-10-02 12:01:30 +0700

    changes on connection.php

--- function/connection.php
+++ function/connection.php
@@ -6,7 +6,7 @@ $dbpass = $_ENV["MYSQLPASSWORD"];
 $dbname = $_ENV["MYSQLDATABASE"];
 $dbport = $_ENV["MYSQLPORT"];
 
-$conn  = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$dbport);
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$dbport);
 
 if($conn->connect_error){
   die('Could not connect connect: '.$conn->connect_error);
